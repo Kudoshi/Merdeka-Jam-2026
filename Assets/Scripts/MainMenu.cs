9787ab61ef598d3661fea57e8ca4118865765e3c
Kudoshi <63440251+Kudoshi@users.noreply.github.com>
2026-08-30 22:07:01 +0800

    feat: main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private CustomButton _startBtn;
+    [SerializeField] private CustomButton _quitBtn;
+
+    private void OnEnable()
+    {
+        _startBtn.onClick.AddListener(StartBtnClicked);
+        _quitBtn.onClick.AddListener(QuitBtnClicked);
+    }
+
+
+    private void StartBtnClicked()
+    {
+        SceneManager.LoadScene("TransitionScene");
+    }
+
+    private void QuitBtnClicked()
+    {
+        Application.Quit();
+    }
+
+
+}
